38f472388bc67d728e20af879a02aa3e7970b033
Zay Min Maw - <zmin@z1t6c3.42bangkok.com>
2025-11-07 20:13:29 +0700

    feat: added count words function

--- src/lexer/lexer_tokenize.c
+++ src/lexer/lexer_tokenize.c
@@ -6,11 +6,35 @@
 /*   By: zmin <zmin@student.42bangkok.com>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/05 22:37:02 by zmin              #+#    #+#             */
-/*   Updated: 2025/11/05 22:46:10 by zmin             ###   ########.fr       */
+/*   Updated: 2025/11/07 20:12:59 by zmin             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "lexer.h"
 #include "utils.h"
 
+static int	count_words(char *str)
+{
+	unsigned int	i;
+	unsigned int	flag;
+	int				count;
 
+	i = 0;
+	flag = 0;
+	count = 0;
+	while (str[i])
+	{
+		ft_skipquotes(str, &i);
+		if (str[i] != ' ')
+			flag = 1;
+		else if (str[i] == ' ' && flag)
+		{
+			flag = 0;
+			count++;
+		}
+		i++;
+	}
+	if (flag)
+		count++;
+	return (count);
+}
