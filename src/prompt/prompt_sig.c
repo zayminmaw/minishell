939e18477fc26913daac3643c51e4e38de8ce47d
Zay Min Maw - <zmin@z2t3c1.42bangkok.com>
2025-11-27 19:04:03 +0700

    fix: mem leaks

--- src/prompt/prompt_sig.c
+++ src/prompt/prompt_sig.c
@@ -6,11 +6,12 @@
 /*   By: zmin <zmin@student.42bangkok.com>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/03 21:05:17 by zmin              #+#    #+#             */
-/*   Updated: 2025/11/26 19:57:26 by zmin             ###   ########.fr       */
+/*   Updated: 2025/11/27 19:03:22 by zmin             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "prompt.h"
+#include "utils.h"
 #include "exit_status.h"
 
 static void	show_new_line(int sig)
@@ -28,6 +29,7 @@ void	override_sig(void)
 {
 	struct sigaction	sa;
 
+	ft_bzero(&sa, sizeof(sa));
 	sa.sa_handler = &show_new_line;
 	sigaction(SIGINT, &sa, NULL);
 	sa.sa_handler = SIG_IGN;
