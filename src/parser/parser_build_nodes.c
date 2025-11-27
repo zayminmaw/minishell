d28d2f2e07879bb45b01af986521a26861c5e0b4
Zay Min Maw - <zmin@z2t3c1.42bangkok.com>
2025-11-27 20:37:05 +0700

    feat: resolve nodes

--- src/parser/parser_build_nodes.c
+++ src/parser/parser_build_nodes.c
@@ -6,7 +6,7 @@
 /*   By: zmin <zmin@student.42bangkok.com>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/25 20:32:40 by zmin              #+#    #+#             */
-/*   Updated: 2025/11/27 20:17:06 by zmin             ###   ########.fr       */
+/*   Updated: 2025/11/27 20:32:52 by zmin             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,5 +18,10 @@ char	**parser_build_nodes(t_node *node, char **tokens, char **envp)
 	tokens = parser_set_inout(node, tokens);
 	if (!parser_resolve_nodes(*tokens, node))
 		node->exec_path = parser_build_path(envp, *tokens);
+	else if (node->node_type == BUILDIN_CHILD
+		|| node->node_type == BUILDIN_PARENT)
+		node->exec_path = ft_strdup(*tokens);
+	else
+		return (++tokens);
 	return (tokens);
 }
