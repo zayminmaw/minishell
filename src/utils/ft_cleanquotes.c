6da2e504291d458c08be5e58863c9a9575c9881d
Zay Min Maw - <zmin@z2t3c1.42bangkok.com>
2025-12-08 20:52:06 +0700

    feat: added more comments

--- src/utils/ft_cleanquotes.c
+++ src/utils/ft_cleanquotes.c
@@ -6,12 +6,15 @@
 /*   By: zmin <zmin@student.42bangkok.com>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/13 19:54:45 by zmin              #+#    #+#             */
-/*   Updated: 2025/11/14 19:53:25 by zmin             ###   ########.fr       */
+/*   Updated: 2025/12/08 20:51:10 by zmin             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "utils.h"
 
+// 1. skip the found quote
+// 2. copy the value until the quote is found
+// 3. skip the end quote
 static void	parse_inside_quote(char *token, char *res, int *i, int *j)
 {
 	char	q;
@@ -23,6 +26,11 @@ static void	parse_inside_quote(char *token, char *res, int *i, int *j)
 		(*i)++;
 }
 
+// remove quotes from token
+// 1. malloc for res str
+// 2. if a quote is found only take the content of the quote
+// 3. else copy as usual
+// 4. null terminate and free old token
 char	*ft_cleanquotes(char *token)
 {
 	char	*res;
