ad73b2db53c82814994840a8febfa02eba309928
zayminmaw <zayminmaw77@gmail.com>
2026-01-08 14:00:19 +0700

    fix: bonus suprise part

--- src/lexer/lexer_expand_var.c
+++ src/lexer/lexer_expand_var.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   lexer_expand_var.c                                 :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: zmin <zmin@student.42bangkok.com>          +#+  +:+       +#+        */
+/*   By: zayminmaw <zayminmaw@student.42.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/12 19:10:07 by zmin              #+#    #+#             */
-/*   Updated: 2025/12/18 20:47:30 by zmin             ###   ########.fr       */
+/*   Updated: 2025/12/27 13:28:42 by zayminmaw        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,15 +14,25 @@
 #include "lexer.h"
 #include "utils.h"
 
-// handle single quote toggle
-// if encounter ' and not in single quote, turn it on
-// if encounter ' and in single quote, turn it off
-static void	handle_quote(char c, int *in_single_quote)
+// handle quote toggle
+// track both single and double quotes
+// single quotes disable expansion, double quotes don't
+static void	handle_quote(char c, int *in_single_quote, int *in_double_quote)
 {
-	if (c == '\'' && !(*in_single_quote))
-		*in_single_quote = 1;
-	else if (c == '\'' && *in_single_quote)
-		*in_single_quote = 0;
+	if (c == '\'' && !(*in_double_quote))
+	{
+		if (*in_single_quote)
+			*in_single_quote = 0;
+		else
+			*in_single_quote = 1;
+	}
+	else if (c == '"' && !(*in_single_quote))
+	{
+		if (*in_double_quote)
+			*in_double_quote = 0;
+		else
+			*in_double_quote = 1;
+	}
 }
 
 // allocate empty string res
@@ -36,17 +46,19 @@ char	*lexer_expand_var(char *token, char **envp)
 {
 	int		i;
 	int		in_single_quote;
+	int		in_double_quote;
 	char	*res;
 	char	*expanded_var;
 
 	i = 0;
 	in_single_quote = 0;
+	in_double_quote = 0;
 	res = ft_strdup("");
 	if (!res)
 		return (NULL);
 	while (token && token[i])
 	{
-		handle_quote(token[i], &in_single_quote);
+		handle_quote(token[i], &in_single_quote, &in_double_quote);
 		if (token[i] == '$' && !in_single_quote)
 		{
 			expanded_var = expand_var(token, &i, envp);
